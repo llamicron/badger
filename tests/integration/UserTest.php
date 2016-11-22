77818000de85e5090f1375de635a097deeb893db
Luke Sweeney <luke@thesweeneys.org>
2016-11-22 15:34:58 -0600

    fixes some formatting in UserTest

--- tests/integration/UserTest.php
+++ tests/integration/UserTest.php
@@ -7,19 +7,19 @@ use App\Counselor;
 
 class UserTest extends TestCase {
 
+// ---------------------------------------------------
+
   use DatabaseMigrations;
 
   /** @test if */
-  public function the_factory_can_create_a_user()
-  {
+  public function the_factory_can_create_a_user() {
     $user = factory(User::class)->create();
     $user->save();
     $this->seeInDatabase('users', ['name' => $user->name]);
   }
 
   /** @test if */
-  public function a_user_can_own_a_counselor()
-  {
+  public function a_user_can_own_a_counselor() {
     // this is literally the same test as CounselorTest::a_counselor_can_belong_to_a_user
     // For verbosity i guess???
     $user = factory(User::class)->create();
